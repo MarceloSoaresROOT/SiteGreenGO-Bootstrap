65538f95104adbba66dc5f75d06fae64ebc1a887
Marcelo Henrique <marcelosoares2005@outlook.com>
2026-03-29 15:24:09 -0300

    V2

--- dados-produtos.js
+++ dados-produtos.js
@@ -11,84 +11,84 @@ const produtos = [
     },
     {
         id: 2,
-        nome: "Banana Prata",
-        preco: "4,50",
+        nome: "Abacaxi Pérola",
+        preco: "3,50",
         imagem: "/imagens/ImagensQuitanda/img/produtos/000002.jpg",
         estoque: "320,5 kg",
-        descricao: "Banana prata da melhor qualidade possível para sua mesa.",
-        descricaoCompleta: "Nossa Banana Prata é cultivada com técnicas sustentáveis, garantindo máxima frescura e sabor. Perfeita para consumo direto ou para receitas."
+        descricao: "Abacaxi Pérola da melhor qualidade possível para sua mesa.",
+        descricaoCompleta: "Nosso Abacaxi Pérola é cultivado com técnicas sustentáveis, garantindo máxima frescura e sabor. Perfeita para consumo direto ou para receitas."
     },
     {
         id: 3,
-        nome: "Banana Prata",
-        preco: "4,50",
+        nome: "Mamao Formosa",
+        preco: "8,50",
         imagem: "/imagens/ImagensQuitanda/img/produtos/000003.jpg",
         estoque: "320,5 kg",
-        descricao: "Banana prata da melhor qualidade possível para sua mesa.",
-        descricaoCompleta: "Nossa Banana Prata é cultivada com técnicas sustentáveis, garantindo máxima frescura e sabor. Perfeita para consumo direto ou para receitas."
+        descricao: "Mamao Formosa da melhor qualidade possível para sua mesa.",
+        descricaoCompleta: "Nossa Mamao Formosa é cultivada com técnicas sustentáveis, garantindo máxima frescura e sabor. Perfeita para consumo direto ou para receitas."
     },
     {
         id: 4,
-        nome: "Banana Prata",
+        nome: "Maça Fuji",
         preco: "4,50",
         imagem: "/imagens/ImagensQuitanda/img/produtos/000004.jpg",
         estoque: "320,5 kg",
-        descricao: "Banana prata da melhor qualidade possível para sua mesa.",
-        descricaoCompleta: "Nossa Banana Prata é cultivada com técnicas sustentáveis, garantindo máxima frescura e sabor. Perfeita para consumo direto ou para receitas."
+        descricao: "Maça Fuji da melhor qualidade possível para sua mesa.",
+        descricaoCompleta: "Nossa Maça Fuji é cultivada com técnicas sustentáveis, garantindo máxima frescura e sabor. Perfeita para consumo direto ou para receitas."
     },
     {
         id: 5,
-        nome: "Banana Prata",
+        nome: "Goaiaba Vermelha",
         preco: "4,50",
         imagem: "/imagens/ImagensQuitanda/img/produtos/000005.jpg",
         estoque: "320,5 kg",
-        descricao: "Banana prata da melhor qualidade possível para sua mesa.",
-        descricaoCompleta: "Nossa Banana Prata é cultivada com técnicas sustentáveis, garantindo máxima frescura e sabor. Perfeita para consumo direto ou para receitas."
+        descricao: "Goaiaba Vermelha da melhor qualidade possível para sua mesa.",
+        descricaoCompleta: "Nossa Goaiaba Vermelha é cultivada com técnicas sustentáveis, garantindo máxima frescura e sabor. Perfeita para consumo direto ou para receitas."
     },
     {
         id: 6,
-        nome: "Banana Prata",
+        nome: "Laranja Lima",
         preco: "4,50",
         imagem: "/imagens/ImagensQuitanda/img/produtos/000006.jpg",
         estoque: "320,5 kg",
-        descricao: "Banana prata da melhor qualidade possível para sua mesa.",
-        descricaoCompleta: "Nossa Banana Prata é cultivada com técnicas sustentáveis, garantindo máxima frescura e sabor. Perfeita para consumo direto ou para receitas."
+        descricao: "Laranja Lima da melhor qualidade possível para sua mesa.",
+        descricaoCompleta: "Nossa Laranja Lima é cultivada com técnicas sustentáveis, garantindo máxima frescura e sabor. Perfeita para consumo direto ou para receitas."
     },
     {
         id: 7,
-        nome: "Banana Prata",
+        nome: "Abacate",
         preco: "4,50",
         imagem: "/imagens/ImagensQuitanda/img/produtos/000007.jpg",
         estoque: "320,5 kg",
-        descricao: "Banana prata da melhor qualidade possível para sua mesa.",
-        descricaoCompleta: "Nossa Banana Prata é cultivada com técnicas sustentáveis, garantindo máxima frescura e sabor. Perfeita para consumo direto ou para receitas."
+        descricao: "Abacate da melhor qualidade possível para sua mesa.",
+        descricaoCompleta: "Nossa Abacate é cultivada com técnicas sustentáveis, garantindo máxima frescura e sabor. Perfeita para consumo direto ou para receitas."
     },
     {
         id: 8,
-        nome: "Banana Prata",
+        nome: "Abacate hass",
         preco: "4,50",
         imagem: "/imagens/ImagensQuitanda/img/produtos/000008.jpg",
         estoque: "320,5 kg",
-        descricao: "Banana prata da melhor qualidade possível para sua mesa.",
-        descricaoCompleta: "Nossa Banana Prata é cultivada com técnicas sustentáveis, garantindo máxima frescura e sabor. Perfeita para consumo direto ou para receitas."
+        descricao: "Abacate hass da melhor qualidade possível para sua mesa.",
+        descricaoCompleta: "Nossa Abacate hass é cultivada com técnicas sustentáveis, garantindo máxima frescura e sabor. Perfeita para consumo direto ou para receitas."
     },
     {
         id: 9,
-        nome: "Banana Prata",
+        nome: "Abóbora Cabotiá",
         preco: "4,50",
         imagem: "/imagens/ImagensQuitanda/img/produtos/000009.jpg",
         estoque: "320,5 kg",
-        descricao: "Banana prata da melhor qualidade possível para sua mesa.",
-        descricaoCompleta: "Nossa Banana Prata é cultivada com técnicas sustentáveis, garantindo máxima frescura e sabor. Perfeita para consumo direto ou para receitas."
+        descricao: "Abóbora Cabotiá da melhor qualidade possível para sua mesa.",
+        descricaoCompleta: "Nossa Abóbora Cabotiá é cultivada com técnicas sustentáveis, garantindo máxima frescura e sabor. Perfeita para consumo direto ou para receitas."
     },
     {
         id: 10,
-        nome: "Banana Prata",
+        nome: "Pepino",
         preco: "4,50",
         imagem: "/imagens/ImagensQuitanda/img/produtos/000010.jpg",
         estoque: "320,5 kg",
-        descricao: "Banana prata da melhor qualidade possível para sua mesa.",
-        descricaoCompleta: "Nossa Banana Prata é cultivada com técnicas sustentáveis, garantindo máxima frescura e sabor. Perfeita para consumo direto ou para receitas."
+        descricao: "Pepino da melhor qualidade possível para sua mesa.",
+        descricaoCompleta: "Nossa Pepino é cultivada com técnicas sustentáveis, garantindo máxima frescura e sabor. Perfeita para consumo direto ou para receitas."
     },
     {
         id: 11,
@@ -101,11 +101,11 @@ const produtos = [
     },
     {
         id: 12,
-        nome: "Banana Prata",
+        nome: "Alho",
         preco: "4,50",
         imagem: "/imagens/ImagensQuitanda/img/produtos/000012.jpg",
         estoque: "320,5 kg",
-        descricao: "Banana prata da melhor qualidade possível para sua mesa.",
-        descricaoCompleta: "Nossa Banana Prata é cultivada com técnicas sustentáveis, garantindo máxima frescura e sabor. Perfeita para consumo direto ou para receitas."
+        descricao: "Alho da melhor qualidade possível para sua mesa.",
+        descricaoCompleta: "Nosso Alho é cultivado com técnicas sustentáveis, garantindo máxima frescura e sabor. Perfeita para consumo direto ou para receitas."
     }
 ];
